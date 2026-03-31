9f89f2c12ba56ed92b4f72d6243deab9b6a9f1f3
BTho1234 <billycthompson1@gmail.com>
2026-03-31 15:53:40 +0100

    31/03/26 - 15:53 Added checks to TripValidationService

--- src/main/java/service/TripValidationService.java
+++ src/main/java/service/TripValidationService.java
@@ -6,7 +6,14 @@ import java.util.List;
 
 public class TripValidationService {
 
-    public void validateTrips(Trips ){
+    public void validateTrips(Trips lastEvent, String newEventType, LocalDateTime newTimestamp){
+
+        if (newEventType == null || newTimestamp == null){
+            throw new IllegalArgumentException("Event type and timestamp must be filled");
+        }
+
+        if (!newEventType)
+
 
     }
 
