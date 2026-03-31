3f1338fa8845f2b038fd0513cded941a02892c99
BTho1234 <billycthompson1@gmail.com>
2026-03-31 16:30:08 +0100

    31/03/26 - 16:29 Added checks to TripValidationService

--- src/main/java/service/TripValidationService.java
+++ src/main/java/service/TripValidationService.java
@@ -2,7 +2,7 @@ package service;
 
 import com.insurance.model.Trips;
 import java.time.LocalDateTime;
-import java.util.List;
+
 
 public class TripValidationService {
 
@@ -12,7 +12,24 @@ public class TripValidationService {
             throw new IllegalArgumentException("Event type and timestamp must be filled");
         }
 
-        if (!newEventType)
+        if (!newEventType.equals("TRIP_START")&& !newEventType.equals("TRIP_END")){
+            throw new IllegalArgumentException("Invalid event type: "+ newEventType);
+        }
+
+        if (lastEvent == null){
+            if (newEventType.equals("TRIP_END")){
+                throw new IllegalArgumentException("Cannot record TRIP_END without prior TRIP_START");
+            }
+            return;
+        }
+
+        if (newTimestamp.isBefore(lastEvent.getEvent_timestamp())){
+            throw new IllegalArgumentException("New event timestamp cannot be before last recorded event");
+        }
+
+        if (lastEvent.getEvent_type().equals("TRIP_START") && newEventType.equals("TRIP_END")){
+
+        }
 
 
 
