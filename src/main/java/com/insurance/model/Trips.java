a34740632bb6e0427ebdc1c355293bfc7600a698
Jacob Adjei <jacob@adjei.co.uk>
2026-03-31 16:07:01 +0100

    31/03/26 - 16:07
    - Created Class file for Trips
    - Should allow TripValidationService to work

--- src/main/java/com/insurance/model/Trips.java
+++ src/main/java/com/insurance/model/Trips.java
@@ -1,6 +1,52 @@
 package com.insurance.model;
 
+import java.time.LocalDateTime;
+
 public class Trips {
+
+
+    private String regNumber;
+    private String event_type;
+    private LocalDateTime event_timestamp;
+    private String location;
+
+    public Trips() {
+    }
+
+    public Trips(String regNumber, String event_type, LocalDateTime event_timestamp, String location) {
+        this.regNumber = regNumber;
+        this.event_type = event_type;
+        this.event_timestamp = event_timestamp;
+        this.location = location;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public String getEvent_type() {
+        return event_type;
+    }
+    public void setEvent_type(String event_type) {
+        this.event_type = event_type;
+    }
+
+    public LocalDateTime getEvent_timestamp() {
+        return event_timestamp;
+    }
+    public void setEvent_timestamp(LocalDateTime event_timestamp) {
+        this.event_timestamp = event_timestamp;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 
 }
