23959977badc12f0d183ae8a537e26412364552e
BTho1234 <billycthompson1@gmail.com>
2026-03-31 14:20:53 +0100

    31/03/26 - 13:02 Added initialisation for DB

--- src/main/java/config/DatabaseConnection.java
+++ src/main/java/config/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/Vehicle%20Insurance%20Compliance%20Tracking%20System";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Billy030707!";
+    private static final String PASSWORD = "(enter password)";
 
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL, USER, PASSWORD);
